24bc456f8db6bc2408fcfaff057aeb5bc34fc3ff
Ngtfury <sreeramachandransmenon@gmail.com>
2022-01-04 18:39:19 +0530

    `number` arg support on `/word` endpoing

--- app.py
+++ app.py
@@ -5,15 +5,30 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route('/')
+def home():
+    return flask.redirect('https://gerty-api.herokuapp.com/home')
+
+@app.route('/home')
 def home():
     return 'Hello world!'
 
-
 @app.route('/word')
 def random_word():
-    word = helper.get_random_word()
-    return str(word)
+    _number = request.args.get('number')
+
+    if _number:
+        try:
+            _int_number = int(_number)
+        except:
+            flask.abort(405)
+    else:
+        _int_number = 1
+
+    word = helper.get_random_word(_int_number)
+    return word
+
 
 
 if __name__ == '__main__':
--- helper.py
+++ helper.py
@@ -1,11 +1,16 @@
 import random
 
-def get_random_word():
+def get_random_word(number=1):
     with open('english_words.txt', 'r') as f:
         _fulltext = f.read()
         _allwords = list(map(str, _fulltext.split('\n')))
 
 
-        return random.choice(_allwords)
+        WORDS = []
+        for x in range(number):
+            _word = random.choice(_allwords)
+            WORDS.append(_word)
+
+        return WORDS
 
 
